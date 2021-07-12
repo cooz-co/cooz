3ef1ef83b65381f91c9a43a08c0ef2d866bb5e9f
cooz-co <85308834+cooz-co@users.noreply.github.com>
2021-07-12 18:08:45 +0200

    Add files via upload

---
+++ v1.2/series-prov.js
@@ -0,0 +1,44 @@
+const seriesProv = [
+		{
+			'name': 'El legado de Júpiter',
+			'year': '2021',
+			'gnr': '<span><b>Ciencia Ficción</b><b>Acción</b><b>Drama</b></span>',
+			'img': '/9yxep7oJdkj3Pla9TD9gKflRApY.jpg',
+			'link': '/#!/93484-1'
+		},
+		{
+			'name': 'The Boys',
+			'year': '2019',
+			'gnr': '<span><b>Ciencia Ficción</b><b>Acción</b></span>',
+			'img': '/rcSXlC1H8Gj7GF6juzeiFhPr9Xv.jpg',
+			'link': '/#!/76479-1'
+		},
+		{
+			'name': 'The Mandalorian',
+			'year': '2019',
+			'gnr': '<span><b>Ciencia Ficción</b><b>Acción</b><b>Western</b></span>',
+			'img': '/iLg3DzrwCDsHNovNaWx5JnQAsiV.jpg',
+			'link': '/#!/82856-1'
+		},
+		{
+			'name': 'Falcon y el Soldado de Invierno',
+			'year': '2021',
+			'gnr': '<span><b>Ciencia Ficción</b><b>Acción</b><b>Western</b></span>',
+			'img': '/bthJ4RYhyvaf5kg4t8Fc8zKkMF0.jpg',
+			'link': '/#!/88396-1'
+		},
+		{
+			'name': 'Bruja Escarlata y Visión',
+			'year': '2021',
+			'gnr': '<span><b>Ciencia Ficción</b><b>Misterio</b><b>Drama</b></span>',
+			'img': '/j2VeSZ7tncd2p8z1CeMnxbxK76b.jpg',
+			'link': '/#!/85271-1'
+		},
+		{
+			'name': 'Loki',
+			'year': '2021',
+			'gnr': '<span><b>Drama</b><b>Ciencia Ficción</b></span>',
+			'img': '/kAHPDqUUciuObEoCgYtHttt6L2Q.jpg',
+			'link': '/#!/84958-1'
+		}
+]
