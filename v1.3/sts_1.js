37347007a56b15d1311d4b8e2f9978d7ce0ad62a
cooz-co <85308834+cooz-co@users.noreply.github.com>
2021-07-30 17:48:01 +0200

    Create sts_1.js

---
+++ v1.3/sts_1.js
@@ -0,0 +1,49 @@
+const servidores = {
+	"doo": {
+		"icon": "https://i.imgur.com/7MT16Fo.png",
+		"name": "Doodstream",
+	},
+	"tap": {
+		"icon": "https://i.imgur.com/B9IBIDk.png",
+		"name": "StreamTape",
+	},
+	"fem": {
+		"icon": "https://i.imgur.com/xE1bCn7.png",
+		"name": "Fembed",
+	},
+	"uq": {
+		"icon": "https://i.imgur.com/V9Itdgb.png",
+		"name": "Uqload",
+	},
+	"waw": {
+		"icon": "https://i.imgur.com/QxAJ5zP.png",
+		"name": "Netu",
+	},
+	"sb": {
+		"icon": "https://i.imgur.com/BFZ5MKF.png",
+		"name": "Streamsb",
+	}
+}
+
+const gateway = [
+	{
+		"url": "https://trusted-setup.filecoin.io/ipfs/",
+		"html": "https://filecoin.io/ipfs/"
+	},
+	{
+		"url": "https://cloudflare-ipfs.com/ipfs/",
+		"html": "https://cloudflare.com/ipfs/"
+	},
+	{
+		"url": "https://video.oneloveipfs.com/ipfs/",
+		"html": "https://video.oneloveipfs.com/ipfs/"
+	},
+	{
+		"url": "https://gateway.ipfs.io/ipfs/",
+		"html": "https://gateway.ipfs.io/ipfs/"
+	},
+	{
+		"url": "https://ipfs.fleek.co/ipfs/",
+		"html": "https://ipfs.fleek.co/ipfs/"
+	}
+]
